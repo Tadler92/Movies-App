f235ebd49787b336da1540c23f648147cde17999
Trevor Duhon <taduhon1485@gmail.com>
2023-12-20 22:00:15 -0600

    Added code that requires ratings to be between 0 & 10, and titles to be more than 2 characters

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 $('#submit-button').on('click', function (e) {
   e.preventDefault();
+  if ($('#movie-rating').val() < 0 || $('#movie-rating').val() > 10) {
+    alert("Ratings must be between 0 and 10");
+    return;
+  }
+  if ($('#movie-title').val().length < 2) {
+    alert('Movie Titles must have at least 2 characters in them.');
+    return;
+  }
   $('table').append(`<tr>
   <td>${$('#movie-title').val()}</td>
   <td>${$('#movie-rating').val()}/10</td>
